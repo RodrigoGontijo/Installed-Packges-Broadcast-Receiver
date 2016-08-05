c31740b7e6c4d130ab6e9d1ab6a7e37faa7bdd19
Rodrigo Gontijo <rodrigo.gontijo@hotmail.com>
2016-08-05 12:38:58 -0300

    new implementation of installPackages Broadcast

--- InstallPackagesBroadcast/app/src/main/java/qira/com/installpackagesbroadcast/InstalledPackagesBroadcast.java
+++ InstallPackagesBroadcast/app/src/main/java/qira/com/installpackagesbroadcast/InstalledPackagesBroadcast.java
@@ -1,11 +1,11 @@
 package qira.com.installpackagesbroadcast;
 
-        import android.content.BroadcastReceiver;
-        import android.content.Context;
-        import android.content.Intent;
-        import android.net.Uri;
-        import android.util.Log;
-        import android.widget.Toast;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.Toast;
 
 public class InstalledPackagesBroadcast extends BroadcastReceiver {
     @Override
@@ -14,9 +14,9 @@ public class InstalledPackagesBroadcast extends BroadcastReceiver {
         if (data.toString().equals("package:" + "com.qira.biometria")) {
             Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage("com.qira.biometria");
             context.startActivity(launchIntent);
-            Toast toast = Toast.makeText(context,"Qira Atulizado com Sucesso!",Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(context, "Qira Atulizado com Sucesso!", Toast.LENGTH_SHORT);
             toast.show();
-            Log.d("QIRA","Qira Atualizou");
+            Log.d("QIRA", "Qira Atualizou");
         }
     }
 }
--- InstallPackagesBroadcast/app/src/main/java/qira/com/installpackagesbroadcast/MainActivity.java
+++ InstallPackagesBroadcast/app/src/main/java/qira/com/installpackagesbroadcast/MainActivity.java
@@ -1,22 +1,43 @@
 package qira.com.installpackagesbroadcast;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TimerTask myTask;
+    private Timer timer;
+    private TextView textView;
+    private PackageInfo pInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        callApp();
+        textView = (TextView) findViewById(R.id.version);
+        try {
+            pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            textView.setText("InstallPackages: " + "V" + pInfo.versionName.toString());
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        callApp();
+        timer = new Timer();
+        myTask = new MyTimerTask();
+        timer.schedule(myTask, 10000);
+
     }
 
 
@@ -24,6 +45,13 @@ public class MainActivity extends AppCompatActivity {
         Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.qira.biometria");
         if (launchIntent != null) {
             startActivity(launchIntent);
+        }
+    }
+
+
+    class MyTimerTask extends TimerTask {
+        public void run() {
+            callApp();
         }
     }
 }
